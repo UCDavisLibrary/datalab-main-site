3f8bd49d5559b9b10c94de8b28ebeb11ebb13260
spelkey-ucd <spelkey@ucdavis.edu>
2023-12-01 10:51:35 -0500

    hummingbird  customizations

--- src/plugins/ucdlib-datalab/includes/main.php
+++ src/plugins/ucdlib-datalab/includes/main.php
@@ -2,6 +2,7 @@
 
 require_once( __DIR__ . '/assets.php' );
 require_once( __DIR__ . '/config.php' );
+require_once( __DIR__ . '/hummingbird.php' );
 require_once( __DIR__ . '/jobs-board/main.php' );
 require_once( __DIR__ . '/robots.php' );
 require_once( __DIR__ . '/timber.php' );
@@ -15,6 +16,7 @@ class UcdlibDatalab {
     $this->utils = new UcdlibDatalabUtils( $this );
 
     $this->assets = new UcdlibDatalabAssets( $this );
+    $this->hummingbird = new UcdlibDatalabHummingbird( $this );
     $this->jobsBoard = new UcdlibDatalabJobsBoard( $this );
     $this->robots = new UcdlibDatalabRobots( $this );
     $this->timber = new UcdlibDatalabTimber( $this );
