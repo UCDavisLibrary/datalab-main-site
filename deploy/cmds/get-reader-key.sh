d5eee500da66d810f2ee3c83bf52fcb6bc082544
spelkey-ucd <spelkey@ucdavis.edu>
2025-03-06 10:23:22 -0500

    update gc key fetch scripts

--- deploy/cmds/get-reader-key.sh
+++ deploy/cmds/get-reader-key.sh
@@ -7,7 +7,6 @@
 set -e
 CMDS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CMDS_DIR/..
-source config.sh
 
 mkdir -p ./secrets
-gcloud --project=digital-ucdavis-edu secrets versions access latest --secret=$GC_READER_KEY_SECRET > ./secrets/gc-reader-key.json
+gcloud --project=digital-ucdavis-edu secrets versions access latest --secret=datalab-main-site-reader > ./secrets/gc-reader-key.json
--- deploy/cmds/get-writer-key.sh
+++ deploy/cmds/get-writer-key.sh
@@ -7,7 +7,6 @@
 set -e
 CMDS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CMDS_DIR/..
-source config.sh
 
 mkdir -p ./secrets
-gcloud --project=digital-ucdavis-edu secrets versions access latest --secret=$GC_WRITER_KEY_SECRET > ./secrets/gc-writer-key.json
+gcloud --project=digital-ucdavis-edu secrets versions access latest --secret=datalab-main-site-writer > ./secrets/gc-writer-key.json
